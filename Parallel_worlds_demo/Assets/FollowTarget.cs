35751186d6fdf78d7028539c46f665f5baa28b53
Cheryl Lao <cheryl@dgp.toronto.edu>
2019-10-29 10:38:25 -0400

    Finally committing the new version with parallel objects

---
+++ Parallel_worlds_demo/Assets/FollowTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+
+    public bool position;
+
+    public Vector3 rotation;
+
+    [Range(0, 1)]
+    public float speed = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (position)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, speed);
+        }
+
+        //if (rotation)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, speed);
+        //}
+    }
+}
