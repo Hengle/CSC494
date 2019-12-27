ab42e4b54c4218623f965f9374185f3717dc0c7b
Cheryl Lao <cheryl@dgp.toronto.edu>
2019-12-27 15:57:58 -0500

    Reassigned voxels to have materials with only 1 colour at runtime

--- Parallel_worlds_demo/Assets/Voxels.cs
+++ Parallel_worlds_demo/Assets/Voxels.cs
@@ -15,22 +15,38 @@ public class Voxels : MonoBehaviour
 
             Mesh mesh = meshFilter.mesh;
 
+            //UV mapping: https://answers.unity.com/questions/516935/how-does-uv-mapping-via-script-work.html
             List<Vector2> uvs = new List<Vector2>();
             mesh.GetUVs(0, uvs);
 
             Vector2 uv = uvs[0];
-            uv.x -= 0.5f;
-            uv.y -= 0.5f;
+            //uv.x -= 0.5f;
+            //uv.y -= 0.5f;
 
-            uv *= 2f;
+            //uv *= 2f;
 
             Color color = texture.GetPixel((int) (texture.width * uv.x), (int) (texture.height * uv.y));
 
-            //Set the colour to something 
-            //renderer.material = new Material(shader);
-            //renderer.material.SetColor("_Color", Color.red);
+            //Save the current shader
+            Shader shader = renderer.material.shader;
+            float glossiness = material.GetFloat("_Glossiness");
+            float metallic = material.GetFloat("_Metallic");
 
+            //Remove the current material
+            Destroy(renderer.GetComponent<Material>());
 
+            print(color*255.0f);
+
+            //Set the colour of the material
+            Material newMaterial = new Material(shader);
+            newMaterial.color = color;
+            newMaterial.SetFloat("_Glossiness", glossiness);
+            newMaterial.SetFloat("_Metallic", metallic);
+
+            Material[] mats = renderer.materials;
+            int numMats = mats.Length;
+            mats[numMats-1] = newMaterial;
+            renderer.materials = mats;
 
             //texture.GetPixel()
         }
