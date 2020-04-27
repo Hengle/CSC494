f25486bcc62825bff42fae9cc16c2b833acfd63c
Cheryl Lao <cheryl.lao@mail.utoronto.ca>
2020-04-26 22:17:19 -0400

    Fixed x axis similarity bugs - need to copy to other axes

--- AttributeSpaces/Assets/Scripts/SimilarityViewManager.cs
+++ AttributeSpaces/Assets/Scripts/SimilarityViewManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba294fbb5413efa8db93cf4e4cd6e996c6430f99ae8242a4444ab30ad270c418
-size 25964
+oid sha256:d10232f495b0672967fee2fda544561b110c4fd85ef6072eaac024c732ce86a5
+size 28365
