5ee88ca17122b8ba95c31d858ff36588efe7a1aa
Cheryl Lao <cheryl.lao@mail.utoronto.ca>
2020-04-29 22:24:15 -0400

    Fixed filter for timeline view

--- AttributeSpaces/Assets/Scripts/DesignSpaceCreator.cs
+++ AttributeSpaces/Assets/Scripts/DesignSpaceCreator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc950a0eabdb6838b74fa7d9acecec108beca384910de25177f0da2ed849a41
-size 2405
+oid sha256:734e1659c4cc6f2abe2d91e5ce7c2d9e76430de1aee256178f8b817f7144002a
+size 2411
--- AttributeSpaces/Assets/VibrationController.cs
+++ AttributeSpaces/Assets/VibrationController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d22b1522b76bf6d8ef37e66c76e70da38e73f44b678d4ebc0ac14d87da63fd4
-size 1340
+oid sha256:96a811e46263e521cd8de91425d143ad0911dddde66bf305ace428f00c6e1720
+size 1555
