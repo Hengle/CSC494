4cc2a48fd28e3ee8e5652a64b8c49432b0a41be1
Cheryl Lao <cheryl.lao@mail.utoronto.ca>
2020-05-01 14:14:16 -0400

    ERROR CS0246 UnityEditorInternal is missing. Also changed the second scene and added reset hotkeys

--- AttributeSpaces/Assets/Scripts/Attribute.cs
+++ AttributeSpaces/Assets/Scripts/Attribute.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3048987da0f544446c387bb03b44a9d332d7e43eeef3678892e7b19cfc569e45
-size 7595
+oid sha256:d15e16d3bb441c674b617b05e6241997e9fbe5c981eac0d009feb002edc1b170
+size 8448
